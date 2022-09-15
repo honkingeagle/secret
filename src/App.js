2c72709981ab202a0c3bccb924f89a5a16d7bfdb
Tiras Mwangi <tmwangi599@gmail.com>
2022-09-15 18:20:13 +0300

    removed buggy gun js code

--- src/App.js
+++ src/App.js
@@ -2,51 +2,16 @@ import TabBar from './components/TabBar';
 import Chat from '../src/components/Chat'
 import TextArea from './components/TextArea';
 import MoreInfo from './components/MoreInfo';
-import {useState, useEffect, useReducer} from 'react'
-import Gun from 'gun';
+import {useState} from 'react'
 function App() {
 
   const [darkMode, setDarkMode] = useState(false)
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [profileName, setProfileName] = useState('Tiras')
   function toggleMode(mode) {
     setDarkMode(mode)
   }
 
-  const gun = Gun({
-    peers: [
-      'http://localhost:5000/gun'
-    ]
-  })
-
-  const initialState = {
-    messages: []
-  }
-
-  function reducer(state, message){
-    return {
-      messages: [message, ...state.messages]
-    }
-  }
-
-  useEffect(() => {
-    const messages = gun.get('messages')
-    messages.map().on(m => {
-      dispatch({
-        name: m.name,
-        message: m.message,
-        createdAt: m.createdAt
-      })
-    })
-  },[])
-
-  function sendMessage(formData) {
-    const messages = gun.get('messages')
-    messages.set({
-        name: 'Tiras',
-        message: formData.message,
-        createdAt: formData.createdAt
-    })
- }
+  
   return (
     <div className="h-screen font-semibold overflow-hidden">
       <div className='h-full grid grid-cols-12'>
@@ -54,14 +19,11 @@ function App() {
           mode={darkMode}
           toggleMode={toggleMode}
         />
-        <Chat
-          chat={state} 
+        <Chat 
           mode={darkMode}
         />
         <TextArea
-          chat={state} 
           mode={darkMode}
-          sendMessage={sendMessage}
         />
         <MoreInfo />
       </div>

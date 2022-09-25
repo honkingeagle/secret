7a8ead7a23aff9766f2f4b850955ffb14b3afd42
Tiras Mwangi <tmwangi599@gmail.com>
2022-09-25 15:16:34 +0300

    removed unnecessary paragraphs

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,8 @@ function Login(props) {
         <div className="h-screen text-center bg-red-50 pt-16">
             <p className="py-4 text-5xl font-bold">Secret</p>
             <div className="text-2xl">
-                <p>Hi there, this is Secret</p>
-                <p>a <strong>Decentralized Messaging Platform</strong></p>
-                <p>Connect your METAMASK</p>
-                <p> Wallet to get started...</p>
+                <p>Connect your <strong>METAMASK</strong> Wallet</p>
+                <p>to get started...</p>
             </div>
             <button className="my-8 py-6 px-8 bg-black text-2xl text-white rounded-md" onClick={props.connectWallet}>
                 Connect Wallet

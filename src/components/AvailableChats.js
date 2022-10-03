e48c2007ceadb65089914f7493fe6b474d820639
Tiras Mwangi <tmwangi599@gmail.com>
2022-10-03 15:58:58 +0300

    add new chats

--- src/components/AvailableChats.js
+++ src/components/AvailableChats.js
@@ -3,7 +3,7 @@ import { ArrowSmLeftIcon, PlusIcon } from "@heroicons/react/outline"
 function AvailableChats(props) {
     return (
         <div className="text-[1.5rem]">
-            <div className="flex justify-between">
+            <div className="flex justify-between border-b-1 border-red-100">
                 <div className="flex items-center ">
                     <button onClick={() => props.isPressed(false)}><ArrowSmLeftIcon className='h-8 w-8 mr-3' /></button>
                     <p>Chats</p>

4b195fd996845a5c0ecfc43c8aa5fecd80368a5f
Tiras Mwangi <tmwangi599@gmail.com>
2022-10-03 16:02:49 +0300

    returned to previous state

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ function Login(props) {
             <button className="my-8 py-6 px-8 bg-black text-2xl text-white rounded-md" onClick={props.connectWallet}>
                 Connect Wallet
             </button>
+            
         </div>
     )
 }

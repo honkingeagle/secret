eeee6c788571b2ffd3af7264e53fcfe41721148b
Tiras Mwangi <tmwangi599@gmail.com>
2022-10-03 16:04:43 +0300

    added saveMessage method and functionality to update ui in real time

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Login from './components/Login'
 import TabBar from './components/TabBar'
 import Chat from './components/Chat'
 import {data} from './Data'
+import Gun from 'gun'
+
+const gun = Gun({
+  peers: [
+    "http://localhost:3030/gun"
+  ],
+})
 
 const ACTION_TYPE = {
   SETDARKMODE: 'setDarkMode',
   SETPROFILENAME: 'setProfileName',
-  SETTEXTTILEPRESSED: 'setIsPressed'
+  SETTEXTTILEPRESSED: 'setIsPressed',
+  SETMESSAGES: 'setAll'
 }
 const reducer = (state, action) => {
     switch(action.type){
@@ -18,6 +26,8 @@ const reducer = (state, action) => {
         return {...state, profileName: action.payload.profileName}
       case ACTION_TYPE.SETTEXTTILEPRESSED:
         return {...state, textTileIsPressed: action.payload.pressed}
+      case ACTION_TYPE.SETMESSAGES:
+        return {...state, messages: [action.payload.messages, ...state.messages]}
       default:
         return state
     }
@@ -47,7 +57,7 @@ function App() {
       if (accounts.length !== 0) {
         const account = accounts[0]
         console.log(`Found an authorized account: ${account}`)
-
+        
         dispatch({type:ACTION_TYPE.SETPROFILENAME, payload: {profileName: account}})
 
       } else {
@@ -79,9 +89,38 @@ function App() {
 
       }
     }
+  useEffect(() => {
+      if (state.profileName) {
+        const messages = gun.get('messages')
+        messages.map(msg => (msg.from === state.profileName || msg.to === state.profileName
+          ) ? msg : 'No available chats'
+        ).once(m => {
+          dispatch({type: ACTION_TYPE.SETMESSAGES, payload: { messages: {
+            from: m.from,
+            to: m.to,
+            message: m.message,
+            createdAt: m.createdAt
+          }
+        }})
+      })
+    }else {
+      console.log("You are not logged in")
+    }
+      
+  },[state.profileName])
+
+  const saveMessage = (formData) => {
+    gun.get('messages').set({
+      from: state.profileName,
+      to: "0x228a5e9502e94cfe7bca43e76da42417c813f04d",
+      message: formData.message,
+      createdAt: "12:45AM"
+    })
+  }
+
   return (
     <div className="max-h-screen tracking-wide overflow-hidden">
-      {console.log(data)}
+      {console.log(state.messages)}
       {state.profileName && ( <div className='h-screen font-semibold grid grid-cols-12'>
         
           <>
@@ -92,12 +131,15 @@ function App() {
             <Chat
               data={data} 
               mode={state.darkMode}
+              messages={state.messages}
+              // logOut={logOut}
               textTileIsPressed={state.textTileIsPressed}
               isPressed={() => dispatch({type:ACTION_TYPE.SETTEXTTILEPRESSED, payload: {pressed: true}})}
             />
             <TextArea
               mode={state.darkMode}
               profileName={state.profileName}
+              saveMessage={saveMessage}
               textTileIsPressed={state.textTileIsPressed}
               isPressed={() => dispatch({type:ACTION_TYPE.SETTEXTTILEPRESSED, payload: {pressed: false}})}
             />
@@ -105,6 +147,7 @@ function App() {
         
       </div>
       )}
+      
       {!state.profileName && (
         <Login 
           connectWallet={connectWallet}
